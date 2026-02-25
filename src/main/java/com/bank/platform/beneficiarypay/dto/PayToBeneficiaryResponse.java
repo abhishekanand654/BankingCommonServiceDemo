f5669f8fdccd1ec0dce49b9029e080ecfe815d25
abhishekanand654 <abhishekanannd654@gmail.com>
2026-02-26 03:52:37 +0530

    removing files

--- src/main/java/com/bank/platform/beneficiarypay/dto/PayToBeneficiaryResponse.java
+++
@@ -1,8 +0,0 @@
-package com.bank.platform.beneficiarypay.dto;
-
-public record PayToBeneficiaryResponse(
-        String beneficiaryId,
-        String paymentId,
-        String status,
-        String correlationId
-) {}
